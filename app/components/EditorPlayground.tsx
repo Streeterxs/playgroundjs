f1cc392274a132273e59a5dc3aca47dd6249c175
Dickson <dicksonmelo@gmail.com>
2024-06-05 05:34:12 -0300

    feat(input-component): create input component

---
+++ app/components/EditorPlayground.tsx
@@ -0,0 +1,15 @@
+import Editor from '@monaco-editor/react';
+
+function EditorPlayground() {
+  return (
+    <Editor
+      height="60vh"
+      width="45vw"
+      defaultLanguage="javascript"
+      defaultValue="console.log('Hello world!')"
+      theme="vs-dark"
+    />
+  );
+}
+
+export default EditorPlayground;
