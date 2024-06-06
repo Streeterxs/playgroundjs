bcecc28bffc7475ce43e1f0d684c789e68062e59
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2024-06-06 16:28:21 -0300

    feat(eslint): typescript, react, globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,26 @@
 import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        React: 'readonly',
+      },
+    },
+    plugins: {
+      react,
+    },
     rules: {
       'no-unused-vars': 'error',
       'no-undef': 'error',
